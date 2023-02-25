47d321801b6e7a5066e7e921ed12f04f07e0a742
ikopke23 <90466377+ikopke23@users.noreply.github.com>
2023-02-25 11:08:29 -0500

    base functions

---
+++ scripts/opinion.js
@@ -0,0 +1,25 @@
+
+//getOpinions
+
+function getRuralOpinion(){
+
+    return opinion;
+}//end Rural Opinion
+
+function getSuburbanOpinion(){
+
+    return opinion;
+}//end Suburban Opinion
+
+function getUrbanOpinion(){
+
+    return opinion;
+}//end Urban Opinion
+
+function greenParty(){
+    
+}
+
+function grayParty(){
+
+}
